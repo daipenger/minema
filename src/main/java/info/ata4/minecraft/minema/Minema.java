4e84a4e4f90540fd676a81ce6203856fb164294b
NyaNLI <nyaplus@qq.com>
2021-05-29 09:00:51 +0800

    prepare for create pull request

--- src/main/java/info/ata4/minecraft/minema/Minema.java
+++ src/main/java/info/ata4/minecraft/minema/Minema.java
@@ -47,7 +47,7 @@ public class Minema {
 
 	public static final String NAME = "Minema";
 	public static final String MODID = "minema";
-	public static final String VERSION = "3.5.4 TickSync";
+	public static final String VERSION = "%VERSION%";
 	public static final String MCVERSION = "1.12.2";
 
 	private static final String category = "key.categories.minema";
